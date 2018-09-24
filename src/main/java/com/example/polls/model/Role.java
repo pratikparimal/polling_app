46060a3fa4006d854a5081ef5768d0377c75ab00
pratikparimal <pratik.parimal@gmail.com>
2018-09-25 02:35:14 +0530

    Model class push

---
+++ src/main/java/com/example/polls/model/Role.java
@@ -0,0 +1,53 @@
+package com.example.polls.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Enumerated(EnumType.STRING)
+	@NaturalId
+	@Column(length = 60)
+	private RoleName role;
+
+	
+	public Role() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public RoleName getRole() {
+		return role;
+	}
+
+
+	public void setRole(RoleName role) {
+		this.role = role;
+	}
+	
+}
